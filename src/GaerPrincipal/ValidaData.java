de87e166b625a620003632b5bd95d22bb6dc388d
gaer <jr.icm.sc@gmail.com>
2018-07-17 11:52:43 -0300

    Primeira Versão

--- src/GaerPrincipal/ValidaData.java
+++ src/GaerPrincipal/ValidaData.java
@@ -1,11 +1,11 @@
 package GaerPrincipal;
 
-import javax.swing.JOptionPane;
+
 
     class ValidaData {
 
         public static void main(String[] args) {
-            Teste
+           
         
     
     
