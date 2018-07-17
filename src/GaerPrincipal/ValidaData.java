1f6a6be25377733acb0d4592d3637bc02f42d22a
gaer <jr.icm.sc@gmail.com>
2018-07-17 16:23:58 -0300

    Primeira Versão

--- src/GaerPrincipal/ValidaData.java
+++ src/GaerPrincipal/ValidaData.java
@@ -1,14 +1,26 @@
 package GaerPrincipal;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 
 
     class ValidaData {
-
-        public static void main(String[] args) {
-           
         
+        public boolean data(String data){
+         try {
+             
+             SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+             sdf.setLenient(false);
+             sdf.parse(data);
+             return true;
+             
+         } catch (ParseException ex) {
+             return false;
+                }
+               
+            }
+       }
     
     
-        }
-    }
     
